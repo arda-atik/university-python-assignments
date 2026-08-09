67cdc4979f3b2166561333dca022ab1219cf69b7
arda-atik <ardaatikk10@gmail.com>
2026-08-09 13:43:36 +0300

    Update collatz_sequence_analyzer.py

--- collatz_sequence_analyzer.py
+++ collatz_sequence_analyzer.py
@@ -1,37 +1,36 @@
-# Read n (first term) and k (the k-th even term) from the user
+# Read inputs
 n = int(input())
 k = int(input())
 
-X = -1  # Largest odd number
-Y = 0   # Number of terms in the interval [10, 100)
-Z = -1  # The k-th even term
+X = -1
+Y = 0
+Z = -1
 
 if n >= 1:
     while True:
-        # X: Update the largest odd number
+        # Update the largest odd number
         if n % 2 == 1 and n > X:
             X = n
-        
-        # Y: Count terms between 10 (inclusive) and 100 (exclusive)
-        if 10 <= n < 100:
-            Y += 1
             
-        # Z: Find the k-th even number
+        # Count terms in interval [10, 100)
+        if 10 <= n < 100:
+            Y = Y + 1
+            
+        # Find the k-th even number
         if n % 2 == 0 and Z == -1:
             if k > 0:
-                k -= 1
+                k = k - 1
                 if k == 0:
                     Z = n
                     
-        # Terminate the loop when the sequence reaches 1
+        # Terminate if the sequence reaches 1
         if n == 1:
             break
             
-        # Collatz Rule: Divide by 2 if even, multiply by 3 and add 1 if odd
+        # DÜZELTME: Sonsuz döngüyü engellemek için n = n // 2 yapıldı.
         if n % 2 == 0:
             n = n // 2
         else:
             n = 3 * n + 1
-
-# Print the results on a single line, separated by a single space
-print(f"{X} {Y} {Z}")
+            
+print(X, Y, Z)
